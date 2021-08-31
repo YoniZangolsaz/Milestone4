8b21169b1545e0350e411db495b7b141ed6b1cc3
israelfrank <israelfrank123@gmail.com>
2021-08-31 14:07:35 +0300

    final update

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,10 @@
 import * as dotenv from 'dotenv';
 
-export const serverPort = 3010;
-export const localHost = 27017;
-export const dbName = 'trainerAndClass';
 dotenv.config();
+
+export const serverPort = process.env.SERVERPORT;
+export const localHost = process.env.LOCALHOST;
+export const dbName = 'trainerAndClass';
 
 const { MONGOIP } = process.env;
 const { MONGOPORT } = process.env;
@@ -11,5 +12,3 @@ export const mongoDbPath = `mongodb://${MONGOIP}:${MONGOPORT}/${dbName}`;
 
 export const trainerCollectionName = 'trainer';
 export const classCollectionName = 'class';
-
-console.log(mongoDbPath);
