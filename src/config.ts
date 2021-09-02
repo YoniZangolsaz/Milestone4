586323d05fd7af6e3198a0873f77a53c4f167979
YoniZangolsaz <yoniza770@gmail.com>
2021-09-02 19:05:35 +0300

    update

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,10 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const serverPort = process.env.SERVER_PORT;
-export const localHost = process.env.LOCAL_HOST;
+export const serverPort = process.env.SERVER_PORT || 3010;
+export const localHost = process.env.LOCAL_HOST || 27017;
+export const APP = process.env.APP || 'app';
+
 export const dbName = 'trainerAndClass';
 
 // const MONGOIP = process.env.MONGOIP;
